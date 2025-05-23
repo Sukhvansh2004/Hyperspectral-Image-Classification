6daf4fba209b3ac1fa68d257a7ef5020a11a6f5b
Sukhvansh Jain <sukhvanshjain@gmail.com>
2025-05-23 23:25:10 +0530

    dump

--- check.py
+++ check.py
@@ -1,4 +1,4 @@
 import scipy.io as sio
 import os   
-data_mat = sio.loadmat("/Users/ashmit22/Hyperspectral-Image-Classification/Cssd Hsi/archive/PaviaU.mat")
-print(data_mat['paviaU'].shape())
+data_mat = sio.loadmat("Cssd Hsi/data/PaviaU/PaviaU.mat")
+print(data_mat['paviaU'].shape)
